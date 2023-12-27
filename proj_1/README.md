9be99959ba1669709ae5f33ea81b9716d21de56d
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-12-27 16:41:19 +0100

    SU proj 1 done, bkp

--- proj_1/README.md
+++ proj_1/README.md
@@ -1,3 +1,13 @@
 # Klasifikace
 
 https://www.kaggle.com/datasets/vjchoudhary7/customer-segmentation-tutorial-in-python
+
+# Vysledky
+
+Zobrazeni vysledku Seskupovani bylo v referenci proti `Annual Income (k$)` a `Spending Score (1-100)`. Je mozne, ze seskupovani dle jinych promennych bude mit jidne vysledky.
+
+Ve vyslednem `fig_Mall_Customers.csv_Gender.png` muzeme videt rozdeleni do 5 skupin. Stredni skupina nebyla pomoci KMeans optimalne rozdelena.
+
+Ve vyslednem `fig_Mall_Customers.csv_all.png` muzeme videt rozdeleni dle Age a Gender do postupne 5 skupin pomoci KMeans. Zde muzeme videt, ze rozdeleni dle Age je pro Age nejviditelnejsi pro pro 5 skupin a rozdeleni dle Gender je nejviditelnejsi dle 4 skupin.
+
+Seskupovani lze nastavit dle prikaldu v `run.sh`
--- proj_1/main.py
+++ proj_1/main.py
@@ -1,61 +1,12 @@
 import getopt
 import sys
-import csv
 from pprint import pprint
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-def parse_csv_to_2d_array(filename):
-    df = pd.read_csv(filename)
-    return df
-
-def train_model(features_train, target_train):
-    print("train")
-    # Train a Classifier
-    classifier = RandomForestClassifier(random_state=42)
-    classifier.fit(features_train, target_train)
-
-    return classifier
-
-def classify_model(classifier, feature_test, target_test):
-    print('classify')
-    target_pred = classifier.predict(feature_test)
-    accuracy = accuracy_score(target_test, target_pred)
-    print(f"Model Accuracy: {accuracy}")
-    pass
-
-def prepare_data(data, target_variable):
-    # Preprocess the Data - Convert categorical variables to numerical
-    label_encoders = {}
-
-    print("Data:")
-    pprint(list(data.columns))
-    print("Target:")
-    print(target_variable)
-    for column in data.columns:
-        label_encoders[column] = LabelEncoder()
-        data[column] = label_encoders[column].fit_transform(data[column])
-
-    # Split the Data to feature and train data
-    #featurs_columns = ['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']
-    #featurs_columns = data.columns.copy(deep=True)
-    featurs_columns = list(data.columns.values)
-    featurs_columns.remove(target_variable)
-    features_data = data[featurs_columns]  # Features
-    target_data = data[target_variable]  # Target variable
-    features_train, features_test, target_train, target_test = train_test_split(
-        features_data, target_data, test_size=0.2, random_state=42)
-
-    return (features_train, features_test, target_train, target_test)
 
 def main(argv):
     input_file = ''
@@ -105,7 +56,6 @@ def main(argv):
     # Read the CSV Data
     data = pd.read_csv(input_file)
 
-
     print("Data:")
     pprint(list(data.columns))
     print("Target:")
@@ -119,47 +69,84 @@ def main(argv):
     if ignore_col is not None:
         data = data.drop(ignore_col, axis='columns')
 
-    # Standardize the data
-    featurs_columns = list(data.columns.values)
-    featurs_columns.remove(target_variable)
-    # Clustering using KMeans
-    kmeans = KMeans(n_clusters=clusters_cnt, random_state=1)
-
-    # Calculate clusters and visualize them in single plot
-    rows_cnt = 1
-    cols_cnt = 2
-    size = 5
-    fig, ax = plt.subplots(rows_cnt, cols_cnt, sharex='col', sharey='row', figsize=(size * cols_cnt, size * rows_cnt))
-    n = cols_cnt * rows_cnt
-    axes = ax.flatten()
-    plot_x = target_variable_x  # 'Annual Income (k$)'
-    plot_y = target_variable_y  # 'Spending Score (1-100)'
-    plt.xlabel(plot_x)
-    plt.ylabel(plot_y)
-    plt.title('Clusters of customers')
-    for i, j in zip(range(n), axes):
-        result_data_name = 'Cluster-' + str(i)
+    orig_target_variable = target_variable
+    if target_variable != "all":
+        # Standardize the data
         featurs_columns = list(data.columns.values)
-
-        target_featurs_columns = list(data.columns.values)
-        target_featurs_columns.remove(target_variable_x)
-        target_featurs_columns.remove(target_variable_y)
-        if len(target_featurs_columns) < i:
-            continue
-        target_variable = target_featurs_columns[i]
-        # if target_variable == target_variable_x or target_variable == target_variable_y:
-        #     continue
         featurs_columns.remove(target_variable)
         scaler = StandardScaler()
         # scaled_features = scaler.fit_transform(df[['Age', 'Annual Income (k$)', 'Spending Score (1-100)']])
         scaled_features = scaler.fit_transform(data[featurs_columns])
 
+        # Clustering using KMeans
+        kmeans = KMeans(n_clusters=clusters_cnt, random_state=1)
+
+        for i in list(data.columns.values):
+            result_data_name = 'Cluster-' + i
+            data[result_data_name] = kmeans.fit_predict(scaled_features)
+
+        result_data_name = "Cluster"
         data[result_data_name] = kmeans.fit_predict(scaled_features)
+        # # Visualize clusters
         data_sum = sum(data[result_data_name])
-        # print("data sum - " + result_data_name + ": " + str(data_sum))
-        print("target: " + target_variable + " data sum - " + result_data_name + ": " + str(data_sum))
-        j.scatter(data[plot_x], data[plot_y], c=data[result_data_name])
-    plt.show()
+        print("data sum - " + result_data_name + ": " + str(data_sum))
+        plot_x = target_variable_x  # 'Annual Income (k$)'
+        plot_y = target_variable_y  # 'Spending Score (1-100)'
+        plt.xlabel(plot_x)
+        plt.ylabel(plot_y)
+        plt.title('Clustered by: ' + target_variable + ' to ' + str(clusters_cnt) + " clusters")
+        plt.scatter(data[plot_x], data[plot_y], c=data[result_data_name])
+        plt.savefig('fig_'+input_file + "_" + target_variable + '.png')
+        plt.show()
+
+    else:
+        # Calculate clusters and visualize them in single plot
+        rows_cnt = clusters_cnt
+        cols_cnt = 2
+        size = 2
+        fig, ax = plt.subplots(rows_cnt, cols_cnt, sharex='col', sharey='row', figsize=(size * cols_cnt, size * rows_cnt))
+        n = cols_cnt * rows_cnt
+        axes = ax.flatten()
+        plot_x = target_variable_x  # 'Annual Income (k$)'
+        plot_y = target_variable_y  # 'Spending Score (1-100)'
+        plt.xlabel(plot_x)
+        plt.ylabel(plot_y)
+        plt.title('Clusters of customers')
+
+        target_features_columns = list(data.columns.values)
+        target_features_columns.remove(target_variable_x)
+        target_features_columns.remove(target_variable_y)
+
+        clusters = {}
+        for i, j in zip(range(n), axes):
+            # Clustering using KMeans to c clusters
+            c = int(i / cols_cnt) + 1
+            kmeans = KMeans(n_clusters=c, random_state=1)
+
+            result_data_name = 'Cl-' + target_variable + "-" + str(c) + '-' + str(i)
+            featurs_columns = list(data.columns.values)
+
+            idx = (i % cols_cnt)
+            if len(target_features_columns) < idx:
+                continue
+            # prepare data
+            target_variable = target_features_columns[idx]
+            featurs_columns.remove(target_variable)
+            scaler = StandardScaler()
+            scaled_features = scaler.fit_transform(data[featurs_columns])
+            # create clusters
+            clusters[result_data_name] = kmeans.fit_predict(scaled_features)
+
+            if verbose:
+                # sum is just for debugging to distinguish different clusters
+                data_sum = sum(clusters[result_data_name])
+                print("target: " + target_variable + " data sum - " + result_data_name + ": " + str(data_sum))
+            # j.set_title(target_variable
+            j.set_title(result_data_name)
+            j.scatter(data[plot_x], data[plot_y], c=clusters[result_data_name], marker='.')
+
+        plt.savefig('fig_'+input_file + "_" + orig_target_variable + '.png')
+        plt.show()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
